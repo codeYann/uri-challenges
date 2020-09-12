a68faa540f013e1c6332c25543644569ea6609cb
codeYann <codeyan10@gmail.com>
2020-09-12 11:37:30 -0300

    Problema 1564: ok

---
+++ src/beginner/1564.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(void) {
+    int N;
+    
+    while(cin >> N) {
+        if(N == 0) {
+            cout << "vai ter copa!" << endl;
+        } else {
+            cout << "vai ter duas!" << endl;
+        }
+
+    }
+ 
+    return 0;
+}
