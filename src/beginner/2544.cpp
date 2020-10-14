709461efa90b00ee27b270c3012c2004ce5aa413
codeYann <codeyan10@gmail.com>
2020-10-14 11:42:50 -0300

    Problema 2544: ok

---
+++ src/beginner/2544.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(void) {
+    long int t;
+
+    while(cin >> t) { 
+        cout << log2(t) << endl;
+    } 
+    
+ 
+    return 0;
+}
